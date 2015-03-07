e512edd0efa3889d46f327adde14ba7e2ed07c88
Abdulsattar Mohammed <asattar.md@gmail.com>
2015-03-07 09:49:21 +0530

    Alternative deploy script

--- go/deploy.sh
+++ go/deploy.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
-echo "Reconstituting SSH key..."
-echo $DEPLOY_KEY | sed 's/\\n/\'$'\n/g' > id_geeknight
-chmod 600 id_geeknight
+#echo "Reconstituting SSH key..."
+#echo $DEPLOY_KEY | sed 's/\\n/\'$'\n/g' > id_geeknight
+#chmod 600 id_geeknight
 
-export GIT_SSH=$(pwd)/go/ssh.sh
-export SSH_KEY=$(pwd)/id_geeknight
+#export GIT_SSH=$(pwd)/go/ssh.sh
+#export SSH_KEY=$(pwd)/id_geeknight
 
-set -xe
+#set -xe
 
-echo "Cloning gh-pages..."
-rm -Rf gh-pages
-git config --global user.email "snap@snap-ci.org"
-git config --global user.name "snap-ci"
-git clone --quiet --branch=gh-pages git@github.com:TWHyderabad/geeknight.git gh-pages
+#echo "Cloning gh-pages..."
+#rm -Rf gh-pages
+#git config --global user.email "snap@snap-ci.org"
+#git config --global user.name "snap-ci"
+#git clone --quiet --branch=gh-pages git@github.com:TWHyderabad/geeknight.git gh-pages
 
+cd $SNAP_WORKING_DIR
 echo "Building site..."
 bundle exec nanoc
 
