e322cdefa8d5157f8dab7b6ac514518ea4fc5d15
Abdulsattar Mohammed <asattar.md@gmail.com>
2015-03-07 09:51:38 +0530

    Add Bundle Install

--- go/deploy.sh
+++ go/deploy.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
 
-#echo "Reconstituting SSH key..."
-#echo $DEPLOY_KEY | sed 's/\\n/\'$'\n/g' > id_geeknight
-#chmod 600 id_geeknight
-
-#export GIT_SSH=$(pwd)/go/ssh.sh
-#export SSH_KEY=$(pwd)/id_geeknight
-
-#set -xe
-
-#echo "Cloning gh-pages..."
-#rm -Rf gh-pages
-#git config --global user.email "snap@snap-ci.org"
-#git config --global user.name "snap-ci"
-#git clone --quiet --branch=gh-pages git@github.com:TWHyderabad/geeknight.git gh-pages
-
 cd $SNAP_WORKING_DIR
+bundle install
 echo "Building site..."
 bundle exec nanoc
 
@@ -30,6 +16,3 @@ echo "Pushing result..."
 git add --all .
 git commit --amend --allow-empty -q -m "Built $SNAP_COMMIT_SHORT"
 git push -fq origin gh-pages
-
-echo "Cleaning up..."
-rm -Rf ~/.ssh/id_geeknight*
