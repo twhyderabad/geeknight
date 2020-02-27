eb545c2e612be9d9cd46b350c0b7ef2be0e9325d
Alice Kile <buoyantair@protonmail.com>
2020-02-27 19:55:29 +0530

    setup: add react-helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>GeekNight - Hyderabad</title>
+      </Helmet>
+      <h1>Hello world</h1>
+    </>
   );
 }
 
